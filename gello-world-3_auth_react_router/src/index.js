4c92ff3f70886047b380498ba142ca364625a08b
JohnWestermeyer <johnwestermeyer@gmail.com>
2021-08-10 14:46:41 -0700

    afterware

--- gello-world-3_auth_react_router/src/index.js
+++ gello-world-3_auth_react_router/src/index.js
@@ -20,7 +20,21 @@ networkInterface.use([{
     req.options.headers['x-refresh-token'] = localStorage.getItem('refreshToken');
     next();
   }
-}]);
+}]); 
+
+networkInterface.useAfter([{
+  applyAfterware({response: {headers}}, next){
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
+    if(token){
+      localStorage.setItem('token', token)
+    }
+    if(refreshToken){
+      localStorage.setItem('refreshToken', refreshToken)
+    }
+    next();
+  }
+}])
 
 const client = new ApolloClient({
   networkInterface: networkInterface
@@ -28,7 +42,7 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <Routes />
+    <Routes /> 
   </ApolloProvider>
 );
 
